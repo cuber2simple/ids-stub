3dedffbfd97eda635c60d18946a5fddc94b06662
cuber <cuber.carrots@gmail.com>
2018-08-26 13:43:19 +0800

    短暂提交

--- stub-base/src/main/java/org/cuber/stub/utils/RpcUtils.java
+++ stub-base/src/main/java/org/cuber/stub/utils/RpcUtils.java
@@ -1,15 +1,11 @@
 package org.cuber.stub.utils;
 
-import org.apache.commons.beanutils.PropertyUtils;
 import org.cuber.stub.StubConstant;
-import org.cuber.stub.conf.Property;
 import org.cuber.stub.rpc.Resp;
-
-import java.util.Map;
 
 public class RpcUtils {
 
-    public static <T> Resp<T> success(T result){
+    public static <T> Resp<T> success(T result) {
         Resp<T> resp = new Resp<>();
         resp.setResult(result);
         resp.setResultCode(StubConstant.SUCCESS_CODE);
@@ -17,17 +13,10 @@ public class RpcUtils {
         return resp;
     }
 
-    public static <T> Resp<T> fail(String resultCode){
+    public static <T> Resp<T> fail(String resultCode) {
         Resp<T> resp = new Resp<>();
         resp.setSuccess(false);
         resp.setResultCode(resultCode);
         return resp;
-    }
-
-    public static void main(String[] args) throws Exception{
-        Property property = new Property();
-        property.setName("com.lang");
-        Map map = PropertyUtils.describe(property);
-        System.out.println(map);
     }
 }

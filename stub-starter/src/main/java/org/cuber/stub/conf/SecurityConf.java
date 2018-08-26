74fdbd26fc8c3ec83e26b045058f2f171b25a392
cuber <cuber.carrots@gmail.com>
2018-08-26 23:01:45 +0800

    更改配置

--- stub-starter/src/main/java/org/cuber/stub/conf/SecurityConf.java
+++ stub-starter/src/main/java/org/cuber/stub/conf/SecurityConf.java
@@ -11,10 +11,12 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class SecurityConf extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
         http.authorizeRequests().antMatchers("/login.htm").anonymous().and()
                 .authorizeRequests().antMatchers("/error/**").anonymous().and()
                 .authorizeRequests().antMatchers("/static/**").anonymous().and()
                 .authorizeRequests().antMatchers("/register.htm/**").anonymous().and()
+                .authorizeRequests().anyRequest().authenticated().and()
                 .formLogin()
                 .loginPage("/login.htm")
                 .loginProcessingUrl("/login")  //very import add

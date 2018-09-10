72918b2e4c98e8768923ecb702784dac5d7d726b
cuber <cuber.carrots@gmail.com>
2018-09-10 10:45:54 +0800

    整理后上发

--- stub-ids-zk/src/main/java/org/cuber/ids/IdGeneratorFactory.java
+++ stub-ids-zk/src/main/java/org/cuber/ids/IdGeneratorFactory.java
@@ -6,14 +6,18 @@ import org.cuber.zk.ZkIdGenerator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.Objects;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class IdGeneratorFactory {
+
     private static Logger logger = LoggerFactory.getLogger(IdGeneratorFactory.class);
     private static long LOCAL_ID = -1l;
     private static ConcurrentHashMap<Class, IdGenerator> map = new ConcurrentHashMap<>();
-
+    private static final String MONTH_FORMATTER = "yyyy_MM";
     public static long _localId() {
         if (-1l == LOCAL_ID) {
             try {
@@ -26,16 +30,33 @@ public class IdGeneratorFactory {
         return LOCAL_ID;
     }
 
-    public static void load(List<IdPattern> ids){
-        if(CollectionUtils.isNotEmpty(ids)){
+    public static void load(List<IdPattern> ids) {
+        if (CollectionUtils.isNotEmpty(ids)) {
             ids.stream().forEach(idPattern ->
-                map.putIfAbsent(idPattern.getDtoClass(),new ZkIdGenerator(idPattern))
+                    map.putIfAbsent(idPattern.getDtoClass(), new ZkIdGenerator(idPattern))
             );
         }
     }
 
-    public static IdGenerator findByClass(Class tClass){
+    public static IdGenerator findByClass(Class tClass) {
         return map.get(tClass);
     }
 
+    public static LocalDateTime thenDatetime(Class tClass, String id) {
+        IdGenerator idGenerator = findByClass(tClass);
+        LocalDateTime localDateTime = null;
+        if (Objects.nonNull(idGenerator)) {
+            localDateTime = idGenerator.findIdTime(id);
+        }
+        return localDateTime;
+    }
+
+    public static String then_yyyy_MM(Class tClass, String id) {
+        LocalDateTime localDateTime = thenDatetime(tClass, id);
+        String returnStr = null;
+        if(Objects.nonNull(localDateTime)){
+            returnStr = localDateTime.format(DateTimeFormatter.ofPattern(MONTH_FORMATTER));
+        }
+        return returnStr;
+    }
 }
---
+++ stub-starter/src/main/java/org/cuber/stub/conf/MybatisConf.java
@@ -0,0 +1,40 @@
+package org.cuber.stub.conf;
+
+import com.github.pagehelper.PageInterceptor;
+import org.apache.ibatis.logging.log4j2.Log4j2Impl;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.boot.autoconfigure.MybatisAutoConfiguration;
+import org.mybatis.spring.boot.autoconfigure.MybatisProperties;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
+import javax.sql.DataSource;
+import java.util.Properties;
+
+@Configuration
+@ConditionalOnClass({SqlSessionFactory.class, SqlSessionFactoryBean.class})
+@ConditionalOnBean({DataSource.class})
+@EnableConfigurationProperties({MybatisProperties.class})
+@AutoConfigureAfter({MybatisAutoConfiguration.class})
+@ConfigurationProperties("mybatis.pageHelper")
+public class MybatisConf extends Properties  {
+    @Autowired(required = false)
+    private SqlSessionFactory sqlSessionFactory;
+
+    @PostConstruct
+    public void addPlugin(){
+        org.apache.ibatis.session.Configuration configuration = sqlSessionFactory.getConfiguration();
+        PageInterceptor pageInterceptor = new PageInterceptor();
+        configuration.setLogImpl(Log4j2Impl.class);
+        pageInterceptor.setProperties(this);
+        configuration.addInterceptor(pageInterceptor);
+    }
+
+}

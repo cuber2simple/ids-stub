5313909b2ee094e05e29c23ef15113ce78b196e2
cuber <cuber.carrots@gmail.com>
2018-09-22 11:11:35 +0800

    远程增加抛出基本错误

--- stub-comp/src/main/java/org/cuber/cache/bridge/CaffeineBridgeContainer.java
+++ stub-comp/src/main/java/org/cuber/cache/bridge/CaffeineBridgeContainer.java
@@ -10,6 +10,8 @@ import org.cuber.stub.rpc.Req;
 import org.cuber.stub.rpc.Resp;
 import org.cuber.stub.util.RpcUtils;
 import org.cuber.stub.vo.StubConfVO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Description;
 
 import java.util.List;
@@ -19,6 +21,9 @@ import java.util.concurrent.ConcurrentHashMap;
 public class CaffeineBridgeContainer<T extends StubConfVO> extends CommonCache<T> {
 
     private ConcurrentHashMap<String, T> settleCaches = new ConcurrentHashMap<>();
+
+    private static Logger logger = LoggerFactory.getLogger(CaffeineBridgeContainer.class);
+
 
     public CaffeineBridgeContainer(ICacheBridge<T> bridge, Class<T> tClass) {
         super(bridge);
@@ -47,10 +52,15 @@ public class CaffeineBridgeContainer<T extends StubConfVO> extends CommonCache<T
         T searchIns = CacheDefUtils.makeSearchIns(key, loadCacheDef(), tClass);
         T result = null;
         if (searchIns != null) {
-            Resp<T> resp = bridge.loadByKey(new Req<>(searchIns));
-            if (RpcUtils.isSuccess(resp)) {
-                result = resp.getResult();
+            try {
+                Resp<T> resp = bridge.loadByKey(new Req<>(searchIns));
+                if (RpcUtils.isSuccess(resp)) {
+                    result = resp.getResult();
+                }
+            } catch (Exception e) {
+                logger.error("远程调用出错,不可能发生", e);
             }
+
         }
         return result;
     }

daa8863ed573608a7a3907d347e7354d4fc68e4b
cuber <cuber.carrots@gmail.com>
2018-09-26 22:01:55 +0800

    修改增加属性问题

--- stub-starter/src/main/java/org/cuber/stub/initializer/AutoZkInitializer.java
+++ stub-starter/src/main/java/org/cuber/stub/initializer/AutoZkInitializer.java
@@ -4,6 +4,10 @@ import org.apache.commons.lang3.StringUtils;
 import org.cuber.stub.StubConstant;
 import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.MapPropertySource;
+
+import java.util.Objects;
 
 public class AutoZkInitializer implements ApplicationContextInitializer {
 
@@ -11,10 +15,17 @@ public class AutoZkInitializer implements ApplicationContextInitializer {
     public void initialize(ConfigurableApplicationContext configurableApplicationContext) {
         String appName = configurableApplicationContext.getId();
         System.setProperty(StubConstant.APP_NAME, appName);
-        PropertyFactory.loadFormZk(appName);
-        PropertyFactory.loadFormZk(StubConstant.COMMON);
+        ConfigurableEnvironment configurableEnvironment = configurableApplicationContext.getEnvironment();
+        MapPropertySource appPropertySource = PropertyFactory.loadFormZk(appName);
+        if (Objects.nonNull(appPropertySource)) {
+            configurableEnvironment.getPropertySources().addLast(appPropertySource);
+        }
+        MapPropertySource commonPropertySource = PropertyFactory.loadFormZk(StubConstant.COMMON);
+        if (Objects.nonNull(commonPropertySource)) {
+            configurableEnvironment.getPropertySources().addLast(commonPropertySource);
+        }
         String center = configurableApplicationContext.getEnvironment().getProperty(StubConstant.CENTER);
-        if(StringUtils.isNotEmpty(center)){
+        if (StringUtils.isNotEmpty(center)) {
             System.setProperty(StubConstant.CENTER, center);
         }
 
